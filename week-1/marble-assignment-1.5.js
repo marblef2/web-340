c1309f53d03d04b7cf27cbf1a8bba12e96ae3de1
Fred Marble <fmarble@my365.bellevue.edu>
2021-01-09 10:01:14 -0600

    Adding Assignment 1.5

---
+++ week-1/marble-assignment-1.5.js
@@ -0,0 +1,31 @@
+/*
+============================================
+; Title:  Marble Assignment 1.5.js
+; Author: Professor Krasso
+; Modified By: Fred Marble
+; Date:   09 January 2021
+; Description:Building a Web Page with NodeJS
+;===========================================
+*/
+
+//Declaring Variables
+let http = require("http");
+
+//Function to process the server request.
+function processRequest(req, res){
+    let body = "Welcome to the Wonderful World of Crocheting!";
+
+    let contentLength = body.length;
+
+    res.writeHead(200,{
+        'Content-Length':contentLength,
+
+        'Content-Type': 'text/plain'
+    });
+
+    res.end(body);
+}
+
+let x = http.createServer(processRequest);
+
+x.listen(8080);
