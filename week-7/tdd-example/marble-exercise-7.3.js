8d621c222b4df2d8b3997955369ec45624d5c5a1
Fred Marble <fmarble@my365.bellevue.edu>
2021-02-20 13:11:37 -0600

    Adding Exercise 7.2

---
+++ week-7/tdd-example/marble-exercise-7.3.js
@@ -0,0 +1,5 @@
+function getFruits(str){
+    return str.split(',');
+}
+
+module.exports = getFruits;
---
+++ week-7/tdd-example/test/marble-exercise-7.2.js
@@ -0,0 +1,8 @@
+let assert = require("assert");
+
+describe("String#split",function(){
+    it("should return an array of fruits",function(){
+        assert(Array.isArray('Apple,Orange,Mango'.split(',')));
+    });
+});
+
